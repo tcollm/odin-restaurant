f03b49d7101bd699bdf0b66e57ae4b3a162377bb
Tyler Collingridge <tylercollingridge@gmail.com>
2024-06-29 15:24:21 -0600

    Add font to css

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("Js worked");
+import './styles.css';
+import foodImage from '../img/food.jpg';
+
+// const body = document.body;
+
+// const heading = document.createElement('h1');
+// heading.textContent = 'Welcome to Gourmet Bistro';
+// body.appendChild(heading);
+
+// const imgElement = document.createElement('img');
+// imgElement.src = foodImage;
+// imgElement.alt = 'Delicious Food';
+// body.appendChild(imgElement);
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,31 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'], 
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource', 
+            },
+        ],
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './src/index.html'
-        })
+            template: './src/index.html', 
+        }),
     ], 
 };
