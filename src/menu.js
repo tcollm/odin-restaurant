59966230b679f09153aa17df3d41bba80cd25a83
Tyler Collingridge <tylercollingridge@gmail.com>
2024-07-02 17:04:56 -0600

    Create HTML content in menu.js
    - Delete menu.html template.

--- src/menu.js
+++ src/menu.js
@@ -4,11 +4,14 @@ import './styles/menu.css';
 export default function generateMenuPage() {
     const content = document.getElementById('content'); 
 
-    const header = document.createElement('h3');
-    header.textContent = 'Menu';
-    content.appendChild(header); 
+    const h3 = document.createElement('h3');
+    h3.textContent = 'Menu';
+    content.appendChild(h3); 
 
+    const hr = document.createElement('hr');
+    content.appendChild(hr); 
 
-    // Menu
-    // Coming soon...
+    const p = document.createElement('p');
+    p.textContent = 'Coming Soon...';
+    content.appendChild(p); 
 }
